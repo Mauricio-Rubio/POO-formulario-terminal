f4cf00bffad712da2f866aa561c9ca8a1fe761ab
Mauricio <mau.r.54@gmail.com>
2021-02-25 16:39:26 -0600

    Crear usuario a partir de terminal listo

--- src/desempeño1/InterfazUsuario.java
+++ src/desempeño1/InterfazUsuario.java
@@ -8,16 +8,24 @@ public class InterfazUsuario {
     Scanner sc = new Scanner(System.in);
 
     public void nuevaPersona() {
-        String nombre;
-        String sexo;
-        String edad;
+    String nombre;
+    int celular;
+    String correo;
+    String sexo;
+    String edad;
+        
         System.out.println("Ingrese su nombre completo");
         nombre = sc.nextLine();
         System.out.println("Ingrese su edad");
         edad = sc.nextLine();
         System.out.println("Ingrese M para masculino, F para femenino");
         sexo = sc.nextLine();
-        usuario = new Persona(nombre, sexo, edad);
+        System.out.println("Ingrese su teléfono celular");
+        celular = sc.nextInt();
+        System.out.println("Ingrese su correo");
+        sc.nextLine();
+        correo = sc.nextLine();
+        usuario = new Persona( nombre,  sexo,  edad, celular,  correo);
         System.out.println(usuario);
     }
 }
--- src/desempeño1/Persona.java
+++ src/desempeño1/Persona.java
@@ -8,15 +8,18 @@ public class Persona {
     String edad;
     Registro folio;
 
+
     
     @Override
     public String toString(){
-        return ""+nombre+" , telefono: "+numero+" , correo; "+correo+" , sexo:"+sexo+" , folio: "+folio; 
+        return ""+nombre+" , telefono: "+numero+" , correo; "+correo+" , sexo: "+sexo+" , folio: "+folio; 
     }
     
-    public  Persona (String nombre, String sexo, String edad){
+    public  Persona (String nombre, String sexo, String edad, int celular, String correo){
         this.nombre = nombre;
         this.sexo = sexo;
         this.edad = edad;
+        numero = celular;
+        this.correo = correo;
     }
 }
