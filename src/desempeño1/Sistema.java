afa94068fbbc7a13f517f7a202080882d011bbbc
Mauricio <mau.r.54@gmail.com>
2021-02-27 15:00:52 -0600

    Corrección en método Bdatos

--- src/desempeño1/Sistema.java
+++ src/desempeño1/Sistema.java
@@ -6,26 +6,27 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Sistema {
+
     Persona Usuario;
-    
-    public void iniciar(){
+
+    public void iniciar() {
         int eleccion = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido al programa de vacunación que NO es político");
         System.out.println("1 para registrarse, 2 para ingresar");
-        eleccion  = sc.nextInt();
-        if( eleccion == 1){
+        eleccion = sc.nextInt();
+        if (eleccion == 1) {
             InterfazUsuario usuario = new InterfazUsuario();
             usuario.nuevaPersona();
             usuario.registrarUsuario(usuario);
             BaseDatos(usuario);
-        }else if(eleccion ==2){
+        } else if (eleccion == 2) {
             System.out.println("En mantenimiento");
-        }else{
+        } else {
             System.out.println("Ingrese una opción válida por favor");
         }
     }
-    
+
     public void BaseDatos(InterfazUsuario usuario) {
         File archivo; //manipula el archivo
         FileWriter escribir; // escribir en el archivo
@@ -37,10 +38,10 @@ public class Sistema {
                 escribir = new FileWriter(archivo, true);
                 linea = new PrintWriter(escribir);
                 //escribir en el archivo
-                linea.println(usuario.usuario.folio.getFolio());
-                linea.println(usuario.usuario.getNombre());
-                linea.println(usuario.usuario.getCorreo());
-                linea.println(usuario.usuario.getNumero());
+                linea.println("Folio: " + usuario.usuario.folio.getFolio());
+                linea.println("Nombre: " + usuario.usuario.getNombre());
+                linea.println("Correo electrónico: " + usuario.usuario.getCorreo());
+                linea.println("Contacto: " + usuario.usuario.getNumero());
                 /*linea.println(usuario.getNombre());
                 linea.println(usuario.getContraseña());//Esta cifrada al acceder al método getContraseña
                 linea.println(usuario.getCuenta().getId());
@@ -57,7 +58,10 @@ public class Sistema {
                 escribir = new FileWriter(archivo, true);
                 linea = new PrintWriter(escribir);
                 //escribir en el archivo
-                linea.println(usuario.usuario.folio.getFolio());
+                linea.println("Folio: " + usuario.usuario.folio.getFolio());
+                linea.println("Nombre: " + usuario.usuario.getNombre());
+                linea.println("Correo electrónico: " + usuario.usuario.getCorreo());
+                linea.println("Contacto: " + usuario.usuario.getNumero());
                 /*linea.println(usuario.getNombre());
                 linea.println(usuario.getContraseña());
                 linea.println(usuario.getCuenta().getId());
