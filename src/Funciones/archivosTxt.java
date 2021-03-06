b25771c01d18beb5d289fe207253e69c27da2cad
Mauricio <mau.r.54@gmail.com>
2021-03-06 17:59:16 -0600

    Actualizar usuario
    Correcta integración del paquete funciones para actualizar al usuario

--- src/Funciones/archivosTxt.java
+++ src/Funciones/archivosTxt.java
@@ -1,10 +1,16 @@
 package Funciones;
 
 import desempeño1.*;
+import java.io.BufferedReader;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class archivosTxt {
 
@@ -44,5 +50,56 @@ public class archivosTxt {
             } catch (Exception e) {
             }
         }
+    }
+        public static Persona buscarFolio(String folio) {
+        File archivo;  //manipular un archivo
+        FileReader leer; //lector
+        String cadena, folioUsuario = "", nombreUsuario = "", correoUsuario = "", contactoUsuario = "", fechaRegistro = "", lugarUsuario = "" ;
+        BufferedReader almacenamiento;
+        archivo = new File("registroVacunación.txt");
+        try {
+            leer = new FileReader(archivo);
+            almacenamiento = new BufferedReader(leer);
+            cadena = "";
+            //usuarioActivo = null;
+            do {
+                try {
+                    cadena = almacenamiento.readLine();
+                    folioUsuario = cadena;
+                    cadena = almacenamiento.readLine();
+                    nombreUsuario = cadena;
+                    cadena = almacenamiento.readLine();
+                    correoUsuario = cadena;
+                    cadena = almacenamiento.readLine();
+                    contactoUsuario = cadena;
+                    cadena = almacenamiento.readLine();
+                    fechaRegistro = cadena;
+                    cadena = almacenamiento.readLine();
+                    lugarUsuario = cadena;
+                    if (cadena != null && folio.equals(folioUsuario)) {
+                        Persona userBusqueda = new Persona(nombreUsuario, correoUsuario, contactoUsuario);
+                        userBusqueda.folio = new Registro(folioUsuario, fechaRegistro, lugarUsuario); 
+                        //usuarioActivo = userBusqueda;
+                        leer.close();
+                        return userBusqueda;
+                    }
+                } catch (IOException ex) {
+                    System.out.println("error encontrar" + ex);
+
+                    Logger.getLogger(Sistema.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } while (cadena != null || (folio.equals(folioUsuario)));
+            try {
+                almacenamiento.close();
+                leer.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Sistema.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return null;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Sistema.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Usuario no existe");
+        return null;
     }
 }
--- src/desempeño1/Sistema.java
+++ src/desempeño1/Sistema.java
@@ -39,26 +39,26 @@ public class Sistema {
             sc.nextLine();
             String folioLogin = "Folio: " + sc.nextLine();
             Persona personaLogin;
-            if (buscarFolio(folioLogin)) {
-                personaLogin = usuarioActivo;
+            if (Funciones.archivosTxt.buscarFolio(folioLogin) != null) {
+                usuarioActivo = Funciones.archivosTxt.buscarFolio(folioLogin);
 
                 //personaLogin.folio.folio=folioLogin;
-                personaLogin.sexo = String.valueOf(folioLogin.charAt(7));
+                usuarioActivo.sexo = String.valueOf(folioLogin.charAt(7));
                 System.out.println("Registro Encontrado");
                 System.out.println("-------------------------------------------------------------------------------");
                 System.out.println("1 para consultar sus datos, 2 para actualizar datos");
                 System.out.println("-------------------------------------------------------------------------------");
                 eleccion = sc.nextInt();
                 if(eleccion == 1){
-                    System.out.println(personaLogin);
+                    System.out.println(usuarioActivo);
                 }else if (eleccion == 2){
                     System.out.println("-------------------------------------------------------------------------------");
                     System.out.println("Ingrese su correo electronico");
                     sc.nextLine();
-                    personaLogin.correo=sc.nextLine();
+                    usuarioActivo.correo=sc.nextLine();
                     System.out.println("Ingrese su numero de contacto");
-                    personaLogin.numero=sc.nextLine();
-                    actualizarUsuario(personaLogin);
+                    usuarioActivo.numero=sc.nextLine();
+                    actualizarUsuario(usuarioActivo);
                 }
                 
                 
@@ -74,70 +74,7 @@ public class Sistema {
         }while(eleccion == 1 || eleccion == 2);
     }
 
-    public boolean buscarFolio(String folio) {
-        File archivo;  //manipular un archivo
-        FileReader leer; //lector
-        String cadena, folioUsuario = "", nombreUsuario = "", correoUsuario = "", contactoUsuario = "", fechaRegistro = "", lugarUsuario = "" ;
-        BufferedReader almacenamiento;
-        archivo = new File("registroVacunación.txt");
-        try {
-            leer = new FileReader(archivo);
-            almacenamiento = new BufferedReader(leer);
-            cadena = "";
-            //usuarioActivo = null;
-            do {
-                try {
-                    cadena = almacenamiento.readLine();
-                    folioUsuario = cadena;
-                    cadena = almacenamiento.readLine();
-                    nombreUsuario = cadena;
-                    cadena = almacenamiento.readLine();
-                    correoUsuario = cadena;
-                    cadena = almacenamiento.readLine();
-                    contactoUsuario = cadena;
-                    cadena = almacenamiento.readLine();
-                    fechaRegistro = cadena;
-                    cadena = almacenamiento.readLine();
-                    lugarUsuario = cadena;
-                    if (cadena != null && folio.equals(folioUsuario)) {
-                        // Persona userBusqueda = new Persona(nombreUsuario, correoUsuario, contactoUsuario , fechaRegistro, folioUsuario);
-                        //Registro registroBusqueda = new Registro(folioUsuario, fechaRegistro, lugarUsuario);
-                        /*if("M".equals(String.valueOf(folioUsuario.charAt(7)))){
-                            sexoUsuario="M";
-                        }else if("F".equals(String.valueOf(folioUsuario.charAt(7)))){
-                            sexoUsuario="SJ";
-                        }*/
-                        Persona userBusqueda = new Persona(nombreUsuario, correoUsuario, contactoUsuario);
-                        userBusqueda.folio = new Registro(folioUsuario, fechaRegistro, lugarUsuario); 
-                        //userBusqueda.folio.folio=folioUsuario;
-                        //userBusqueda.folio.fecha=fechaRegistro;
-                        //userBusqueda.folio.lugar=lugarUsuario;                        
-                        //System.out.println(userBusqueda.getNumero());
-                        usuarioActivo = userBusqueda;
-                        //System.out.println(usuarioActivo);
-                        leer.close();
-                        //System.out.println(userBusqueda);
-                        return true;
-                    }
-                } catch (IOException ex) {
-                    System.out.println("error encontrar" + ex);
 
-                    Logger.getLogger(Sistema.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            } while (cadena != null || (folio.equals(folioUsuario)));
-            try {
-                almacenamiento.close();
-                leer.close();
-            } catch (IOException ex) {
-                Logger.getLogger(Sistema.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return true;
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Sistema.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("Usuario no existe");
-        return false;
-    }
 
 
 
